d51803c6c039bc2ec90b973df4c2eede3e69c471
Sue Xiong <sxiong0519@gmail.com>
2021-10-25 09:51:12 -0500

    shutting down..

--- PryVata/Controllers/InformationController.cs
+++ PryVata/Controllers/InformationController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PryVata.Models;
+using PryVata.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,57 @@ namespace PryVata.Controllers
     [ApiController]
     public class InformationController : ControllerBase
     {
+        private readonly IInformationRepository _informationRepository;
+
+        public InformationController(IInformationRepository informationRepository)
+        {
+            _informationRepository = informationRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var information = _informationRepository.GetAllInformation();
+
+            return Ok(information);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var information = _informationRepository.GetInformationById(id);
+            if (information == null)
+            {
+                return NotFound();
+            }
+            return Ok(information);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Information information)
+        {
+            _informationRepository.AddInformation(information);
+            return CreatedAtAction("Get", new { id = information.Id }, information);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Information information)
+        {
+            if (id != information.Id)
+            {
+                return BadRequest();
+            }
+
+            _informationRepository.UpdateInformation(information);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _informationRepository.DeleteInformation(id);
+            return NoContent();
+        }
     }
 }
--- PryVata/Controllers/RecipientController.cs
+++ PryVata/Controllers/RecipientController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PryVata.Models;
+using PryVata.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,57 @@ namespace PryVata.Controllers
     [ApiController]
     public class RecipientController : ControllerBase
     {
+        private readonly IRecipientRepository _recipientRepository;
+
+        public RecipientController(IRecipientRepository recipientRepository)
+        {
+            _recipientRepository = recipientRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var recipient = _recipientRepository.GetAllRecipients();
+
+            return Ok(recipient);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var recipient = _recipientRepository.GetRecipientById(id);
+            if (recipient == null)
+            {
+                return NotFound();
+            }
+            return Ok(recipient);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Recipient recipient)
+        {
+            _recipientRepository.AddRecipient(recipient);
+            return CreatedAtAction("Get", new { id = recipient.Id }, recipient);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Recipient recipient)
+        {
+            if (id != recipient.Id)
+            {
+                return BadRequest();
+            }
+
+            _recipientRepository.UpdateRecipient(recipient);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _recipientRepository.DeleteRecipient(id);
+            return NoContent();
+        }
     }
 }
